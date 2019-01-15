54f2c3f85b6fb0fa0409a7a0ff58f802616b2e9f
Sergey Shirnin <nexron171@gmail.com>
2019-01-15 12:30:30 +0300

    Initial Commit

---
+++ TestAlg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestAlg
+//
+//  Created by Sergey Shirnin on 15/01/2019.
+//  Copyright © 2019 Sergey Shirnin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestAlg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestAlg
+//
+//  Created by Sergey Shirnin on 15/01/2019.
+//  Copyright © 2019 Sergey Shirnin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
